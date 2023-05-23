854ef80d9b158cd87268e124403bae92700be0ae
omar214 <omarmohamed2322000@gmail.com>
2023-05-23 06:47:18 +0300

    run test cases

--- test_cases/expressions/expressions.cpp
+++ test_cases/expressions/expressions.cpp
@@ -1,28 +1,11 @@
 void main() {
   int x = 345;
+
   print(x);
   {
+    print(x);
     int x = 1;
     print(x);
   }
   print(x);
-
-
-  // string s = "hello";
-  // char c = 'c';
-  // float f = 3.14;
-  // bool b = true;
-  // bool fa = false;
-  // print(c);
-  // print(f);
-  // print(s);
-  // print(b);
-  // print(fa);
-
-  // int y = 10;
-  // int z = x / y - x;
-  // { int x = 1; }
-  // bool a = true;
-  // bool b = false;
-  // bool c = a && b;
 }
